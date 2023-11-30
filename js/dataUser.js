fb73ff048a7902b3a505d3ad840cd1a11cef54ce
Romina Carranza <romii.carranza@gmail.com>
2023-11-29 21:59:22 -0300

    registro de nuevo usuario

--- js/dataUser.js
+++ js/dataUser.js
@@ -1,26 +1,14 @@
+
 const acceso = document.getElementById("acceso")
 const bienvenida = document.getElementById("bienvenidaUser")
 const formAcceso = document.getElementById("formAcceso")
 
+const userName = document.getElementById("userName")
+const userLastName = document.getElementById("userLastName")
+const passNewUser = document.getElementById("passwordUserNew")
+
 const btnEnviar = document.getElementById("btnEnviar")
 
-
-// async function validarEmail(email){
-//     try {
-//         const response = await fetch(`http://127.0.0.1:5000/login/${email}`)
-//         const data = await response.json()
-        
-//         return data
-      
-//       } catch (error) {
-//         console.error('Error al obtener platos con pastas:', error)
-//         return null;
-// }
-// }
-
-    
-// /[a-z0-9._%\-]+@[a-z]+\.[a-z][a-z]+$/i  
-    
 
 // Función de validación de dirección de correo electrónico
 function validarEntrada(input) {
@@ -28,7 +16,7 @@ function validarEntrada(input) {
     const entrada = input.value
     
     validez = regex.test(entrada)
-    
+
     if (validez) {
         input.classList.remove("inputNoValido") 
         return true
@@ -48,22 +36,6 @@ function validarPass(input) {
         return false
     }
 }
-
-// try {
-//     const response = await fetch(`http://127.0.0.1:5000/login/${entrada}`)
-//     const data = await response.json();
-//     if (data.datos) {
-//         input.classList.remove("inputNoValido") 
-//         return true
-//     } else {
-//         input.classList.add("inputNoValido")
-//         return false 
-//     }     
-//   } catch (error) {
-//     console.error('Error al obtener cliente:', error)
-//     return false;
-// }
-
 
 
 async function handlerValidarIngreso() {      
@@ -86,7 +58,15 @@ async function handlerValidarIngreso() {
     return userRegistrado
 }
 
-
+function completeField(input) {
+    let campo = input.value.trim()
+    if ( campo.length <= 0) {
+        input.classList.add("inputNoValido")
+    } else {
+        input.classList.remove("inputNoValido")
+    }
+}
+   
 
 // userRegistrado = handlerValidarIngreso();
 userLogueado = validarLogin();
@@ -100,4 +80,33 @@ if (userLogueado ) {
          mostrar(bienvenida , false)
 }
 
-  
+function enviarFormulario(event) {
+    event.preventDefault()
+
+    const formData = {
+        "nombre" : document.getElementById('userName').value,
+        "apellido" : document.getElementById('userLastName').value,
+        "clave" : document.getElementById('passwordUserNew').value,
+        "email" : document.getElementById('userIdNew').value,
+        "fecha_nac": document.getElementById('fechaNac').value,
+        "tel": document.getElementById('tel').value,
+        "id_restaurante": "1"
+    }
+
+    // Enviar datos al servidor en formato JSON
+    fetch('http://127.0.0.1:5000/page/acceso-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        window.location.href = "../index.html"
+    })
+    .catch(error => {
+        console.error('Error al enviar datos:', error)
+    });
+}
+
