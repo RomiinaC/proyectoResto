cf81532b59d000eabc49a11a5d3d9f0c503a10c9
Romina Carranza <romii.carranza@gmail.com>
2023-12-02 16:37:47 -0300

    agregando comprobaciones al form-contact

--- js/contacto.js
+++ js/contacto.js
@@ -33,6 +33,26 @@ atras.addEventListener("click",retrocederFoto)
 
 renderizarImagen()
 
+function completeField(input) {
+    let campo = input.value.trim()
+    if ( campo.length <= 0) {
+        input.classList.add("inputNoValido")
+    } else {
+        input.classList.remove("inputNoValido")
+    }
+}
 
+function validarEntrada(input) {
+    const regex = /[a-z0-9._%\-]+@[a-z]+\.[a-z][a-z]+$/i  
+    const entrada = input.value
+    
+    validez = regex.test(entrada)
 
-
+    if (validez) {
+        input.classList.remove("inputNoValido") 
+        return true
+    } else {
+        input.classList.add("inputNoValido") 
+        return false
+    }
+}
